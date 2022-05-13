731a83415b15af236a8dbd51dbbe4583ab294c37
Maria Meyer <mbean1216@icloud.com>
2022-05-12 23:40:58 -0500

    creating voting routes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,51 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // creating associations
+
+// user has many posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
+// posts belongs to user
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+// user belongs to many posts
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+// post belongs to many users
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// vote belongs to user
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// vote belongs to post
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// user has many votes
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+// post has many votes
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
